93e676b856b787a84470a8339b44b90a43ce5626
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-12-03 21:10:25 +0530

    Create Pattern12.cpp

---
+++ Pattern12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter n here: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            char ch = 'A' + i + j - 2;
+            cout <<ch<< " ";
+           
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
