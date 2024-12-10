1ef48e96c6ed19b118012815b88502d4b23782da
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-12-10 20:44:15 +0530

    Update Pattern9.cpp

--- Pattern9.cpp
+++ Pattern9.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int n;
     cout << "Enter n here: ";
-    cin >> n;
+    cin >> n; 
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
